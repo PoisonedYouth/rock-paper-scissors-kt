e15127ea04ad3b353a00fb87e49191b737d83280
Matthias Schenk <matthias.schenk@sevdesk.de>
2023-09-01 13:52:48 +0200

    build: Configured dependencies and plugins.
    - ktlint (for unified code formatting)
    - detekt (for static code analysis)
    - jacoco (for code coverage calculation)
    - junit5 + kotest (for testing)
    - pitest (for mutation testing)
    - archunit (for enforcing architecture)
    
    Using gradle version catalog for handling all versions of plugins, dependencies and jdk. Also using version catalog update plugin to automatically update versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,79 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    kotlin("jvm") version "1.9.0"
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.versionUpdate)
+    alias(libs.plugins.catalogUpdate)
+    alias(libs.plugins.pitest)
+    jacoco
+    application
 }
 
-group = "org.example"
+group = "com.poisonedyouth"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+application {
+    mainClass.set("com.poisonedyouth.rps.MainKt")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    // logback
+    implementation(libs.logback)
+
+    // arrow
+    implementation(platform(libs.arrowStack))
+    implementation(libs.arrowCore)
+
+    // testing
+    testImplementation(libs.junit5Engine)
+    testImplementation(libs.mockitoKotlin)
+    testImplementation(libs.kotestArrowAssert)
+    testImplementation(libs.kotestCoreAssertion)
+    testImplementation(libs.kotestRunnerJunit5)
+    testImplementation(libs.archUnitJunit5)
+}
+
+repositories {
+    mavenCentral()
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
+val jdkVersion: String = libs.versions.jdkVersion.get()
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain {
+        this.languageVersion.set(JavaLanguageVersion.of(jdkVersion))
+    }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+}
+
+pitest {
+    setProperty("junit5PluginVersion", libs.pitestJunit5.get().version)
+    setProperty("testPlugin", "junit5")
+    setProperty("targetClasses", listOf("com.poisonedyouth.rps.*"))
+    setProperty("mutationThreshold", 10)
+    setProperty("outputFormats", listOf("HTML"))
+    setProperty("threads", 8)
+    setProperty("withHistory", true)
+    setProperty("failWhenNoMutations", false)
+    setProperty("useClasspathFile", true)
+    setProperty("jvmArgs", listOf("-Xmx16G"))
+    setProperty("avoidCallsTo", listOf("kotlin.jvm.internal"))
 }
